36e135b65091f6cb7b94c54b08d00f191c57a4ed
Sirgalas <hudos@rambler.ru>
2019-10-07 09:03:54 +0300

    lesson 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Lesson from './02_components/lesson.jsx';
-import CounterButton from './03_state/lesson.jsx';
+import CounterButton from './04_props/lesson.jsx';
 import registerServiceWorker from './registerServiceWorker';
 // Syntax without JSX
 // React.createElement('div', null, 'Simple div with text');
