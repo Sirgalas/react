cf5065b138696271561c4b16906cfc9cc5f2754a
Sirgalas <hudos@rambler.ru>
2019-10-09 09:08:56 +0300

    lesson 6.1

---
+++ src/06_handling_events/lesson.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropsTypes from 'prop-types';
+
+
+const MyLInk =({text,onClick})=>(
+    <a href="/test" onClick={onClick}>{text}</a>
+);
+
+MyLInk.propTypes={
+    text:PropsTypes.string,
+    onClick: PropsTypes.func,
+}
+
+MyLInk.defaultProps={
+    text:'link',
+    onClick: ()=>{},
+}
+
+class App extends Component{
+    handleClick = (e) =>{
+        e.preventDefault();
+        console.log('Clicked on link!')
+    }
+    render(){
+        return (
+            <MyLInk onClick={(e)=>this.handleClick(e,'test')}/>
+        )
+    }
+}
+export default App
