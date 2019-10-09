98296f587f6c8ed1093c86430cde7c60138338bf
Sirgalas <hudos@rambler.ru>
2019-10-09 23:55:30 +0300

    lesson 8

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Lesson,Counter,Button} from './05_prop_types/lesson.jsx';
+import App from './08_lists_keys/lesson.jsx';
 import registerServiceWorker from './registerServiceWorker';
 // Syntax without JSX
 // React.createElement('div', null, 'Simple div with text');
 // React.createElement('input', { className: 'just-class' });
-ReactDOM.render(<Lesson child={<Button />}><Counter/></Lesson>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
