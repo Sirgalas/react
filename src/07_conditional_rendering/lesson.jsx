a67d9279422057ea006ab6875d0c185647120cfd
Sirgalas <hudos@rambler.ru>
2019-10-09 23:34:10 +0300

    lesson 7.2

--- src/07_conditional_rendering/lesson.jsx
+++ src/07_conditional_rendering/lesson.jsx
@@ -10,8 +10,14 @@ const ValidationMsg = ({val})=>{
 }
 
 const App =()=>{
+    const val=9;
     return (
-        <ValidationMsg val={8} />
+        <div>
+            {/*ternary*/}
+            {val>=10?<h2>Grate than 10</h2>:<h3>Less than <em>10</em></h3>}
+            {/* &&*/}
+            {val && <span>We have val</span>}
+        </div>
     );
 }
 export default App
