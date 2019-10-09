0313848ae5f14b811e18b73bd2e9157ae83cb732
Sirgalas <hudos@rambler.ru>
2019-10-09 23:49:18 +0300

    lesson 7.3

--- src/07_conditional_rendering/lesson.jsx
+++ src/07_conditional_rendering/lesson.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component} from 'react';
 import PropsTypes from 'prop-types';
 
 const ValidationMsg = ({val})=>{
@@ -9,15 +9,37 @@ const ValidationMsg = ({val})=>{
     }
 }
 
-const App =()=>{
-    const val=9;
-    return (
-        <div>
-            {/*ternary*/}
-            {val>=10?<h2>Grate than 10</h2>:<h3>Less than <em>10</em></h3>}
-            {/* &&*/}
-            {val && <span>We have val</span>}
-        </div>
-    );
+const Tab1 = () =>(
+    <h1>Text of tab1</h1>
+)
+const Tab2 = () =>(
+    <h1>Text of tab2</h1>
+)
+const Tab3 = () =>(
+    <h1>Text of tab3</h1>
+)
+
+class App extends Component{
+    state = {
+        activeTab:1
+    }
+    handleTab = (e) =>{
+        this.setState({
+            activeTab: +e.target.getAttribute('data-name'),
+        })
+    }
+  render() {
+        const{activeTab} =this.state
+      return (
+          <Fragment>
+              <button data-name={1} onClick={this.handleTab}>Tab1</button>
+              <button data-name={2} onClick={this.handleTab}>Tab2</button>
+              <button data-name={3} onClick={this.handleTab}>Tab3</button>
+              {activeTab === 1&&<Tab1/>}
+              {activeTab === 2&&<Tab2/>}
+              {activeTab === 3&&<Tab3/>}
+          </Fragment>
+      );
+  }
 }
 export default App
