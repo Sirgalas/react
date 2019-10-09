a5afe1076582b01397185aebb0785de889006a5a
Sirgalas <hudos@rambler.ru>
2019-10-09 23:53:54 +0300

    lesson 7 end

--- src/07_conditional_rendering/lesson.jsx
+++ src/07_conditional_rendering/lesson.jsx
@@ -35,10 +35,12 @@ class App extends Component{
               <button data-name={1} onClick={this.handleTab}>Tab1</button>
               <button data-name={2} onClick={this.handleTab}>Tab2</button>
               <button data-name={3} onClick={this.handleTab}>Tab3</button>
-              {activeTab === 1&&<Tab1/>}
-              {activeTab === 2&&<Tab2/>}
-              {activeTab === 3&&<Tab3/>}
+              {activeTab === 1?<Tab1/>:activeTab === 2?<Tab2/>:<Tab3/>}
+              <div>
+                  {`Active tab is: ${activeTab===1 ? 'first':activeTab===2 ? 'second':'third'}`}
+              </div>
           </Fragment>
+
       );
   }
 }
