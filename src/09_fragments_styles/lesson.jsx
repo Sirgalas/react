7291cf8677e7ee8ac382d9ac409ece66627f5531
Sirgalas <hudos@rambler.ru>
2019-10-11 09:09:20 +0300

    lesson 9 arrays

--- src/09_fragments_styles/lesson.jsx
+++ src/09_fragments_styles/lesson.jsx
@@ -1,7 +1,18 @@
 import React, { Fragment, Component} from 'react';
 import PropsTypes from 'prop-types';
 
+const Columns =()=>([
+        <td key="1">Hello</td>,
+        <td key="2">World</td>
+    ])
+
 const Table = () =>(
-    <div>!!!</div>
+    <table>
+        <tbody>
+            <tr>
+            <Columns/>
+            </tr>
+        </tbody>
+    </table>
 );
 export default Table;
