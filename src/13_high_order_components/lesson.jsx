4a1c6768f092d4764ce7c32aa0646e68d9001959
Sirgalas <hudos@rambler.ru>
2019-10-16 09:39:51 +0300

    lesson 13.1

--- src/13_high_order_components/lesson.jsx
+++ src/13_high_order_components/lesson.jsx
@@ -1,86 +1,27 @@
 import React, { Component, Fragment} from 'react';
+import {BrowserRouter as Router,Link} from 'react-router-dom';
 
-const POSITIONS = [
-    {
-        id:'fd',
-        value:'Front-end Developer',
-        title:'Front-end Developer',
-    },
-    {
-        id:'bd',
-        value:'Back-end Developer',
-        title:'Back-end Developer',
-    }
-]
+const AppLink=({to,children}) => ({
+    render:() =>(
+        <Link to={to} activeClassName="active">
+            {children}
+        </Link>
+    )
+})
 
-class Form extends Component{
-    state ={
-        inputText:'',
-        textareaText:'',
-        selectText:'',
-        showData:{
-            data:'',
-            name:'',
-            position:''
-        }
-    }
-    inputRef =React.createRef();
-    textareaRef =React.createRef();
-    selectRef =React.createRef();
+class Lesson extends Component{
 
-    handleChange=()=>{
-        this.state({
-            inputText:this.inputRef.current.value,
-            textareaText:this.textareaRef.current.value,
-            selectText:this.selectRef.current.value
-        })
-    }
-
-    handleShow =(e) => {
-        e.preventDefault();
-        const  {inputText,textareaText,selectText} = this.state;
-        this.setState({
-            inputText: '',
-            textareaText:'',
-            selectText:'',
-            showData:{
-                name:inputText,
-                text:textareaText,
-                position: selectText
-            }
-        })
-    }
-    getRef = (node) => {this.el = node};
     render(){
-        const {inputText,textareaText,selectText,showData} = this.state;
-        const {name,text,position} = showData;
-        return(
-            <Fragment>
-                <form>
-                    <label>
-                        <input ref={this.inputRef} type="text" name="name" value={inputText} onChange={this.handleChange}/>
-                    </label>
-                    <br/>
-                    <label htmlFor="text">Text:</label>
-                    <textarea ref={this.textareaRef} id="text" value={textareaText} onChange={this.handleChange}/>
-                    <br/>
-                    <select ref={this.selectRef} value={selectText} onChange={this.handleChange}>
-                        {
-                            POSITIONS.map(({id, value, title})=>
-                                (
-                                    <option key={id} value={value}>{title}</option>
-                                )
-                            )}
 
-                    </select>
-                    <br/>
-                    <button onClick={this.handleShow}>Show</button>
-                </form>
-                <h2>{name}</h2>
-                <h3>{text}</h3>
-                <h3>{position}</h3>
-            </Fragment>
-        )
+        return(
+            <Router>
+                <nav>
+                    <AppLink to="/">Home</AppLink>
+                    <AppLink to="/portfolio">Portfolio</AppLink>
+                    <AppLink to="/contacts" >Contacts</AppLink>
+                </nav>
+            </Router>
+        );
     }
 }
-export default Form;
+export default Lesson;
