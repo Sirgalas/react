79889587513d954b60ce4e30dabd1fd1505960c3
Sirgalas <hudos@rambler.ru>
2019-10-16 10:46:52 +0300

    lesson 13 end

--- src/13_high_order_components/lesson.jsx
+++ src/13_high_order_components/lesson.jsx
@@ -1,27 +1,39 @@
 import React, { Component, Fragment} from 'react';
-import {BrowserRouter as Router,Link} from 'react-router-dom';
+//import { BrowserRouter as Router, Link } from 'react-router-dom';
+import LoadingHOC from './HOC/loadingHoc';
 
-const AppLink=({to,children}) => ({
-    render:() =>(
-        <Link to={to} activeClassName="active">
-            {children}
-        </Link>
-    )
-})
+class AppComponentUI extends Component {
+    render() {
+        return(
+                <div>{this.props.data.title}</div>
+            );
+    }
+}
+
+const AppComponent =LoadingHOC('data')(AppComponentUI);
 
 class Lesson extends Component{
+    state = {
+        data:{},
+}
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response=>response.json())
+            .then(data =>this.updateState(data))
+    }
+    updateState = (data)=>{
+        window.setTimeout(()=>{
+            this.setState({data})
+        },3000)
+    }
     render(){
 
         return(
-            <Router>
-                <nav>
-                    <AppLink to="/">Home</AppLink>
-                    <AppLink to="/portfolio">Portfolio</AppLink>
-                    <AppLink to="/contacts" >Contacts</AppLink>
-                </nav>
-            </Router>
+            <AppComponent data = {this.state.data} />
         );
     }
 }
+
+
 export default Lesson;
