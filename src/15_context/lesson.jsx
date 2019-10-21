b855b53ecf785e4cd8e0cb8b767e96e0394bd549
Sirgalas <hudos@rambler.ru>
2019-10-21 09:08:22 +0300

    lesson 15 end

--- src/15_context/lesson.jsx
+++ src/15_context/lesson.jsx
@@ -1,10 +1,15 @@
-import React, { Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 
 const  TitleContext = React.createContext();
 const LevelThree=()=>(
     <TitleContext.Consumer>
-        {title =><h1>{title}</h1>}
+        {({title,subTitle,click}) =>
+        <Fragment>
+            <h1 onClick={click}>{title}</h1>
+            <h2>{subTitle}</h2>
+        </Fragment>
+        }
     </TitleContext.Consumer>
     )
 const LevelTwo=()=><LevelThree  />
@@ -14,7 +19,7 @@ class Lesson extends Component{
 
     render() {
         return(
-            <TitleContext.Provider value="Simple title">
+            <TitleContext.Provider value={{title:"Simple title",subTitle:"Sub Title", click:()=>{console.log('Hello!')}}}>
                 <LevelOne />
             </TitleContext.Provider>
         )
