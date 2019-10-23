16519d7d3bfbc2140e57416498f2e7a2678f4cd6
Sirgalas <hudos@rambler.ru>
2019-10-23 09:02:18 +0300

    lesson 19 start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './18_routers_one/Lesson';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from "./18_routers_one/components/home/home";
+import Home from "./19_routers_two/components/home/home";
 import Contacts from './18_routers_one/components/contacts/contacts';
 import Posts from "./18_routers_one/components/posts/posts";
 
